8a4b94b00ea3df873e7e747224d16ed82c5d317b
Joanna Hsiao <chsiao58@gmail.com>
2019-05-29 08:41:13 -0400

    passed all test

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,23 +4,68 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+
+    Character[][] board;
     public Board(Character[][] matrix) {
+        board = matrix;
     }
 
+    /*
+    * 00 | 01 | 02
+    * ---+----+---
+    * 10 | 11 | 12
+    * ---+----+---
+    * 20 | 21 | 22
+    * */
+
     public Boolean isInFavorOfX() {
-        return null;
+        if (isWinning('X'))
+            return true;
+        return false;
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+        if (isWinning('O'))
+            return true;
+        return false;
     }
 
     public Boolean isTie() {
-        return null;
+        if(!isBoardFull())
+            return false;
+        if (isBoardFull() && (isWinning('O') || isWinning('X')))
+            return false;
+        return true;
     }
+
 
     public String getWinner() {
-        return null;
+        if (isWinning('X'))
+            return "X";
+        if (isWinning('O'))
+            return "O";
+        return "";
     }
 
+    private Boolean isWinning(Character player) {
+        if(     board[0][0].equals(player) && board[0][1].equals(player) && board[0][2].equals(player) ||
+                board[1][0].equals(player) && board[1][1].equals(player) && board[1][2].equals(player) ||
+                board[2][0].equals(player) && board[2][1].equals(player) && board[2][2].equals(player) ||
+                board[0][0].equals(player) && board[1][0].equals(player) && board[2][0].equals(player) ||
+                board[0][1].equals(player) && board[1][1].equals(player) && board[2][1].equals(player) ||
+                board[0][2].equals(player) && board[1][2].equals(player) && board[2][2].equals(player) ||
+                board[0][0].equals(player) && board[1][1].equals(player) && board[2][2].equals(player) ||
+                board[0][2].equals(player) && board[1][1].equals(player) && board[2][0].equals(player))
+            return true;
+        return  false;
+    }
+
+    private Boolean isBoardFull() {
+        for (Character[] Horizontal : board)
+            for (Character space : Horizontal)
+                if (space.equals(' '))
+                    return false;
+        return true;
+    }
 }
+
